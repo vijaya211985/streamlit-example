022fd45640c826ff839a945569477e98cf891ce1
vijaya211985 <vijaymmu@gmail.com>
2022-10-22 13:49:33 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -1,9 +1,9 @@
 
-import seaborn as sns
+
 import streamlit as st
 import numpy as np
 import pandas as pd
-
+import seaborn as sns
 
 iris = sns.load_dataset('iris')
 fig = plt.figure(figsize=(10, 4))
