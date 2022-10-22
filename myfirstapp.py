2d3f20efd90a497e8211a934bebf2e02af2ebd30
vijaya211985 <vijaymmu@gmail.com>
2022-10-22 15:56:39 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -1,9 +1,10 @@
-
 import streamlit as st
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 iris = sns.load_dataset('iris')
+fig = plt.figure(figsize=(10, 4))
 sns.boxplot(data=iris)
-
+st.pyplot(fig)
